d2b0ad66c837b0e4c3571744773cf1b64ae749fb
nicolett10 <torrestapia.n@gmail.com>
2022-09-05 20:32:58 +0000

    randcard

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
 /* eslint-disable */
-import "bootstrap";
+// import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let icon = null;
+  let prevIcon = null;
+  setInterval(function() {
+    icon = generaterandomtype();
+    console.log(icon);
+    document.querySelector(".card").innerHTML = generaterandomnumber();
+    if (prevIcon != null) {
+      document.querySelector(".card").classList.remove(prevIcon);
+    }
+    document.querySelector(".card").classList.add(icon);
+    prevIcon = icon;
+  }, 1000);
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generaterandomtype = () => {
+  let type = ["diamond", "spade", "heart", "club"];
+  let indextype = Math.floor(Math.random() * type.length);
+  return type[indextype];
+};
+
+let generaterandomnumber = () => {
+  let numbers = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexnumb = Math.floor(Math.random() * numbers.length);
+  return numbers[indexnumb];
 };
